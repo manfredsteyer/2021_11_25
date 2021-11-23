71b233dd36ca015b8e47a1deef55064fd4fc38ef
Manfred Steyer <manfred.steyer@gmx.net>
2021-11-23 16:21:47 +0100

    day1 afternoon

---
+++ libs/luggage/feature-checkin/src/lib/luggage-feature-checkin.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LuggageDomainModule } from '@flight-workspace/luggage/domain';
+import { CheckinComponent } from './checkin.component';
+// import { FakeFlightService } from '@flight-workspace/flight-lib'
+
+@NgModule({
+  imports: [CommonModule, LuggageDomainModule],
+  declarations: [CheckinComponent],
+  exports: [CheckinComponent],
+})
+export class LuggageFeatureCheckinModule {}
+
+
+
+
+// Manfred was here!! You've been haaaacked!!!
+
+// One more change!
